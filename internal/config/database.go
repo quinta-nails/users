b0fb41fadef566877d5e9308243a47db7d09d5ab
jonkofee <jonkofee@icloud.com>
2024-09-10 04:49:44 +0300

    Add detailed URL field comment in DatabaseConfig struct
    This commit adds comments to the URL field in the DatabaseConfig struct, providing detailed information about its purpose and usage. It clarifies that the URL is sourced from the DATABASE_URL environment variable and includes a default value for fallback.

--- internal/config/database.go
+++ internal/config/database.go
@@ -3,5 +3,10 @@ package config
 import "net/url"
 
 type DatabaseConfig struct {
+
+	// URL is the configuration field for the database URL. It is used by the DatabaseConfig struct
+	// to define the URL for the database connection. The URL is fetched from the environment variable
+	// DATABASE_URL, and if not found, it falls back to the default value:
+	// "postgres://postgres:postgres@localhost:5432/users?sslmode=disable".
 	URL url.URL `env:"DATABASE_URL,required" envDefault:"postgres://postgres:postgres@localhost:5432/users?sslmode=disable"`
 }
