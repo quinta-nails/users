113f8ac39e31f41a3a2b34f7a282ec84fe2aeb68
jonkofee <jonkofee@icloud.com>
2024-09-16 17:13:29 +0300

    typpo

--- internal/config/server.go
+++ internal/config/server.go
@@ -3,7 +3,7 @@ package config
 type ServerConfig struct {
 
 	// Port represents the port number on which the server is listening.
-	// It is an unsigned integer of 8 bits (uint8).
+	// It is an unsigned integer of 16 bits (uint16).
 	// The value of the port can be set using the environment variable PORT.
 	// If the environment variable is not set, the default value is 51051.
 	Port uint16 `env:"PORT,required" envDefault:"51051"`
